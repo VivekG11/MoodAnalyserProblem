629456bd7dac0b79ccd52c11b6e254a71ae4ee41
VivekG11 <vivekvk2903@gmail.com>
2021-07-18 23:57:05 +0530

    [Creating object for patameterized constructor]

--- MoodAnalyzerProblem/MoodAnalyserFactory.cs
+++ MoodAnalyzerProblem/MoodAnalyserFactory.cs
@@ -31,6 +31,33 @@ namespace MoodAnalyzerProblem
                 throw new CustomException(CustomException.ExceptionType.CONSTRUCTOR_NOT_FOUND, "Constructor Not Found");
             }
         }
-        
+
+        public string ParameterizedMoodAnalyser(string className, string constructorName, string message)
+        {
+            try
+            {
+                Type type = typeof(MoodAnalyser);
+                if (type.Name.Equals(constructorName) || type.FullName.Equals(className))
+                {
+                    if (type.Name.Equals(constructorName))
+                    {
+                        ConstructorInfo info = type.GetConstructor(new[] { typeof(string) });
+                        var obj = info.Invoke(new object[] { message });
+                        return Convert.ToString(obj);
+                    }
+
+                    else
+                    {
+                        throw new CustomException(CustomException.ExceptionType.CONSTRUCTOR_NOT_FOUND, "Constructor Not Found");
+
+                    }
+                }
+            }
+            catch(Exception)
+            {
+                throw new CustomException(CustomException.ExceptionType.CLASS_NOT_FOUND, "Class Not Found");
+            }
+            return default;
+        }
     }
 }
